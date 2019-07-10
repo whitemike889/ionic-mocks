a92369e24f573baef33678c56b21a02c88af2fd9
yalexhwang <y.alex.hwang@gmail.com>
2019-07-10 17:15:28 -0400

    Updating ModalController: adding a new method for ionic4

--- src/angular/modal-controller.ts
+++ src/angular/modal-controller.ts
@@ -3,8 +3,12 @@ import { ModalMock } from './modal';
 
 export class ModalControllerMock {
     public static instance(modalMock?: ModalMock): any {
-        let instance = createSpyObj('ModalController', ['create']);
+        let instance = createSpyObj('ModalController', [
+            'create',
+            'present',
+        ]);
         instance.create.and.returnValue(modalMock || ModalMock.instance());
+        instance.present.and.returnValue(modalMock || ModalMock.instance());
 
         return instance;
     }
