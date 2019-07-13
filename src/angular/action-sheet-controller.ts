a39502c806ed55abf50c19ce9d4997dfecb40a4e
yalexhwang <y.alex.hwang@gmail.com>
2019-07-13 01:27:08 -0400

    Add a new method 'present' to ActionSheetController for ionic 4

--- src/angular/action-sheet-controller.ts
+++ src/angular/action-sheet-controller.ts
@@ -4,8 +4,9 @@ import { ActionSheetMock } from './action-sheet';
 export class ActionSheetControllerMock {
     public static instance(actionSheet?: ActionSheetMock): any {
 
-        let instance = createSpyObj('ActionSheetController', ['create']);
+        let instance = createSpyObj('ActionSheetController', ['create', 'present']);
         instance.create.and.returnValue(actionSheet || ActionSheetMock.instance());
+        instance.present.and.returnValue(Promise.resolve());
 
         return instance;
     }
